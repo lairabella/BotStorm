b56b22f51bcc35d06690aee775a244118075b86c
Morgana <morgana@Morgana.morgana>
2019-03-07 18:45:17 -0300

    Cambios

--- run_bot.py
+++ run_bot.py
@@ -5,15 +5,22 @@ from telegram.ext import Updater, CommandHandler
 def start(bot, update):
   update.message.reply_text("I'm a bot, Nice to meet you!")
 
+def talk(bot, update):
+  update.message.reply_text("How are you?")
+
 def main():
   # Create Updater object and attach dispatcher to it
   updater = Updater('624266682:AAGGxwa_ZRSzDdJknM9gpCryDvmvhItebkY')
   dispatcher = updater.dispatcher
   print("Bot started")
 
+
   # Add command handler to dispatcher
-  start_handler = CommandHandler('start',start)
-  dispatcher.add_handler(start_handler)
+
+  dispatcher.add_handler(CommandHandler("start", start))
+
+  dispatcher.add_handler(CommandHandler("talk", talk))
+
 
   # Start the bot
   updater.start_polling()
